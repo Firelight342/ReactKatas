8cd3adb08e426e8def6fca9bd092f968da18af9b
Kat <Katrinabefus@gmail.com>
2022-01-12 12:01:28 -0600

    CharacterCreator hide buttons and save favorite combo, React

--- src/DressUpGame.tsx
+++ src/DressUpGame.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import './DressUpGame.css'
 
-let hatImgs = ['./Hats/hat1.png', './Hats/hat2.png']
-let shirtImgs = ['./Hats/shirt1.png', './Hats/shirt2.png']
+let hatImgs = ['./Hats/hat1.png', './Hats/hat2.png', './Hats/hat1.png', './Hats/hat2.png', './Hats/hat1.png', './Hats/hat2.png', './Hats/hat1.png', './Hats/hat2.png']
+let shirtImgs = ['./Hats/shirt1.png', './Hats/shirt2.png', './Hats/shirt1.png', './Hats/shirt2.png', './Hats/shirt1.png', './Hats/shirt2.png', './Hats/shirt1.png', './Hats/shirt2.png'
+    , './Hats/shirt1.png', './Hats/shirt2.png', './Hats/shirt1.png', './Hats/shirt2.png', './Hats/shirt1.png', './Hats/shirt2.png']
 
 interface ButtonProps {
     itemName: string,
     imgUrls: string[],
     onClick: (imgUrl: string) => void
 }
-class ClothingButtons extends React.Component<ButtonProps, any>{
+interface ButtonState {
+    isTrayOpen: boolean
+}
+class ClothingButtons extends React.Component<ButtonProps, ButtonState>{
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isTrayOpen: false
+        }
+    }
+
     render(): React.ReactNode {
+        var inverse = !this.state.isTrayOpen;
+        // boolean ? "if true" : "if false"
         return (
             <>
-                <h1>{this.props.itemName}</h1>
-                {this.props.imgUrls.map((imgUrl: string) => {
+                <h1 onClick={() => this.setState({ isTrayOpen: inverse })}>
+                    {this.state.isTrayOpen ? "-" : "+"}{this.props.itemName}</h1>
+                {this.state.isTrayOpen && this.props.imgUrls.map((imgUrl: string) => {
                     return (
 
                         <img
@@ -28,35 +42,71 @@ class ClothingButtons extends React.Component<ButtonProps, any>{
         );
     }
 }
+interface Favorite {
+    hatUrl: string
+    shirtUrl: string
+    name: string
+}
 
 interface DU1State {
     hatUrl: string
     shirtUrl: string
+    favoriteInput: string
+    favorites: Favorite[]
 }
 export class DressUpGameTryingStuff extends React.Component<any, DU1State> {
     constructor(props: any) {
         super(props);
         this.state = {
+            favoriteInput: "",
+            favorites: [],
             hatUrl: "",
             shirtUrl: ""
         }
     }
+
+    renderFavoriteListItem(favorite: Favorite): React.ReactNode {
+        return (
+            <li onClick={() => this.setState({ hatUrl: favorite.hatUrl, shirtUrl: favorite.shirtUrl })}>
+                {favorite.name}
+            </li>);
+    }
+
+    saveFavorite(): void {
+        this.setState({
+            favoriteInput: "",
+            favorites: [...this.state.favorites,
+            {
+                name: this.state.favoriteInput,
+                hatUrl: this.state.hatUrl,
+                shirtUrl: this.state.shirtUrl
+            }]
+        })
+    }
+
     render(): React.ReactNode {
+        console.log(this.state)
         return (
             <>
-                <div className="clothesTray">
-                    <ClothingButtons itemName='Hats' imgUrls={hatImgs} onClick={(url) => this.setState({ hatUrl: url })} />
-                    <ClothingButtons itemName='Shirts' imgUrls={shirtImgs} onClick={(url) => this.setState({ shirtUrl: url })} />
-                </div>
-                <div style={{ width: "70%" }}>
-                    <div>
-                        <img src={this.state.hatUrl} className="smallImg" />
+                <div className='stage'>
+                    <div className="clothesTray">
+                        <ClothingButtons itemName='Hats' imgUrls={hatImgs} onClick={(url) => this.setState({ hatUrl: url })} />
+                        <ClothingButtons itemName='Shirts' imgUrls={shirtImgs} onClick={(url) => this.setState({ shirtUrl: url })} />
                     </div>
-                    <div>
-                        <img src={this.state.shirtUrl} style={{ height: "120px" }} />
+                    <div style={{ width: "70%" }}>
+                        <div>
+                            <img src={this.state.hatUrl} className="smallImg" />
+                        </div>
+                        <div>
+                            <img src={this.state.shirtUrl} style={{ height: "120px" }} />
+                        </div>
+                        <input type="text"
+                            value={this.state.favoriteInput}
+                            onChange={(e) => this.setState({ favoriteInput: e.target.value })} />
+                        <button onClick={() => this.saveFavorite()} >Save</button>
+                        <ul>{this.state.favorites.map(this.renderFavoriteListItem)}</ul>
                     </div>
                 </div>
-
 
             </>
         )
